34598ad3cf6fbe0f96c19a53e75fc40870b73ed9
Jalithc <126810658+Jalithc@users.noreply.github.com>
2024-05-05 09:58:21 +0530

    Add files via upload

---
+++ create_const.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+a = tf.constant([2], name = 'constant_a')
+b = tf.constant([3], name = 'constant_b')
+
+@tf.function
+def add(a,b):
+    c = tf.add(a, b)
+    #c = a + b is also a way to define the sum of the terms
+    print(c)
+    return c
+
+result = add(a,b)
+tf.print(result[0])
